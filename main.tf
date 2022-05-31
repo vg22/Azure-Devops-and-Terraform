a7da6003bd500927929cd3fb1ed1b0fd58e8b5c2
vg22 <vgdeveops22@gmail.com>
2022-05-31 14:09:30 -0400

    first commit

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ provider "azurerm" {
     features {}
 }
 
-terraform {
-    backend "azurerm" {
-        resource_group_name  = "tf_rg_blobstore"
-        storage_account_name = "tfstoragebinarythistle"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
-}
+# terraform {
+#     backend "azurerm" {
+#         resource_group_name  = "tf_rg_blobstore"
+#         storage_account_name = "tfstoragebinarythistle"
+#         container_name       = "tfstate"
+#         key                  = "terraform.tfstate"
+#     }
+# }
 
 variable "imagebuild" {
   type        = string
@@ -20,28 +20,28 @@ variable "imagebuild" {
 
 
 resource "azurerm_resource_group" "tf_test" {
-  name = "tfmainrg"
-  location = "Australia East"
+  name = "tfmainrg22"
+  location = "eastus"
 }
 
-resource "azurerm_container_group" "tfcg_test" {
-  name                      = "weatherapi"
-  location                  = azurerm_resource_group.tf_test.location
-  resource_group_name       = azurerm_resource_group.tf_test.name
+# resource "azurerm_container_group" "tfcg_test" {
+#   name                      = "weatherapi"
+#   location                  = azurerm_resource_group.tf_test.location
+#   resource_group_name       = azurerm_resource_group.tf_test.name
 
-  ip_address_type     = "public"
-  dns_name_label      = "binarythistlewa"
-  os_type             = "Linux"
+#   ip_address_type     = "public"
+#   dns_name_label      = "binarythistlewa"
+#   os_type             = "Linux"
 
-  container {
-      name            = "weatherapi"
-      image           = "binarythistle/weatherapi:${var.imagebuild}"
-        cpu             = "1"
-        memory          = "1"
+#   container {
+#       name            = "weatherapi"
+#       image           = "binarythistle/weatherapi:${var.imagebuild}"
+#         cpu             = "1"
+#         memory          = "1"
 
-        ports {
-            port        = 80
-            protocol    = "TCP"
-        }
-  }
-}
+#         ports {
+#             port        = 80
+#             protocol    = "TCP"
+#         }
+#   }
+# }
