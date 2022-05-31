edd6d948e46fd45779cea32b568988e63539ca2a
vg22 <vgdeveops22@gmail.com>
2022-05-31 14:26:05 -0400

    run terraform

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ provider "azurerm" {
     features {}
 }
 
-# terraform {
-#     backend "azurerm" {
-#         resource_group_name  = "tf_rg_blobstore"
-#         storage_account_name = "tfstoragebinarythistle"
-#         container_name       = "tfstate"
-#         key                  = "terraform.tfstate"
-#     }
-# }
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rg_blobstore22"
+        storage_account_name = "tfstoragevgdevops"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
 
 variable "imagebuild" {
   type        = string
